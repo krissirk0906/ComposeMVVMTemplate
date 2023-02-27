f0d5a90b3e9e2ab763d03a817b47040f58125a64
Cao Thành Tâm <tam.ct@gmail.com>
2023-02-27 11:48:09 +0700

    implement material3

--- app/src/main/java/com/juneNine/composeWithMVVM/ui/home/HomeScreen.kt
+++ app/src/main/java/com/juneNine/composeWithMVVM/ui/home/HomeScreen.kt
@@ -6,7 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -14,10 +21,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.juneNine.composeWithMVVM.ui.theme.AppTheme
-import com.juneNine.composeWithMVVM.ui.weather_card.WeatherCard
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    val isChecked = remember { mutableStateOf(false) }
     AppTheme() {
         Column(
             modifier = Modifier
@@ -27,6 +34,19 @@ fun HomeScreen(navController: NavController) {
             Text(text = "Home Screen", modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 25.dp), textAlign = TextAlign.Center, fontSize = 20.sp)
+            Switch(
+                checked = isChecked.value,
+                onCheckedChange = { /*...*/ },
+                thumbContent = {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "checked"
+                    )
+                },
+            )
+            ExtendedFloatingActionButton(onClick = { /* do something */ }) {
+                Text(text = "Extended FAB")
+            }
         }
     }
 }
